e316402baa12c375a5fdbd7dcc82293367f28b34
John Flight <jflight@guidewire.com>
2016-10-25 09:43:07 -0700

    notes_json files

--- src/main/scala/jsonb/rsb/RsbNoteWebUtils.scala
+++ src/main/scala/jsonb/rsb/RsbNoteWebUtils.scala
@@ -74,65 +74,16 @@ object RsbNoteWebUtils {
 
   def main(args: Array[String]): Unit = {
     val books = Books.allBooksSortedLargestToSmallest
-    writeFile(books(16))
-    writeFile(books(17))
-    writeFile(books(18))
 
+    val start = 55
+    val howMany = 11
 
-/*
-16	2chronicles	822
-17	1kings	816
-18	1samuel	810
-19	2kings	719
-20	2samuel	695
-21	mark	678
-22	joshua	658
-23	judges	618
-24	1corinthians	437
-25	romans	433
-26	nehemiah	406
-27	revelation	404
-28	daniel	357
-29	hebrews	303
-30	ezra	280
-31	2corinthians	257
-32	ecclesiastes	222
-33	zechariah	211
-34	hosea	197
-35	esther	167
-36	ephesians	155
-37	lamentations	154
-38	galatians	149
-39	amos	146
-40	song	117
-41	1timothy	113
-42	james	108
-43	micah	105
-44	1peter	105
-45	1john	105
-46	philippians	104
-47	colossians	95
-48	1thessalonians	89
-49	ruth	85
-50	2timothy	83
-51	joel	73
-52	2peter	61
-53	habakkuk	56
-54	malachi	55
-55	zephaniah	53
-56	jonah	48
-57	nahum	47
-58	2thessalonians	47
-59	titus	46
-60	haggai	38
-61	philemon	25
-62	jude	25
-63	obadiah	21
-64	3john	14
-65	2john	13
+    for (i <- start until start + howMany) {
+      val book = books(i)
+      writeFile(book)
+      println("Finished " + book.oneYearBibleName)
+    }
 
-
- */
   }
 }
 
