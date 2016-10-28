f743ac7201d7d32f008f89724d5584963db66a5f
John Flight <jflight@guidewire.com>
2016-10-28 14:37:22 -0700

    HghNivBooks

--- src/main/scala/jsonb/niv/HghNivBooks.scala
+++ src/main/scala/jsonb/niv/HghNivBooks.scala
@@ -1,6 +1,7 @@
 package jsonb.niv
 
-import jsonb.Books
+import jsonb.{Utils, Books}
+import play.api.libs.json.{Json, JsObject}
 
 
 /**
@@ -8,13 +9,27 @@ import jsonb.Books
   */
 object HghNivBooks {
 
-/*
   lazy val allBooks: Seq[NivBook] = {
+    val json: String = Utils.resourceFileToString("/niv/hgh_niv.json")
+    val allBooksJsObject: JsObject = Json.parse(json).as[JsObject]
+
     Books.allBooks
       .map(book => {
-
+        val bookJsObject: JsObject = (allBooksJsObject \ book.hghNivName).as[JsObject]
+        HghNivBookParser(book).fromJson(bookJsObject)
       })
   }
-*/
+
+
+  def main(args: Array[String]): Unit = {
+    val nivBook: NivBook = allBooks.filter(nivBook => nivBook.book.codeName == "romans").head
+
+    val json: String = NivBookParser.toJson(nivBook)
+//    println(json)
+
+    val nivBook2: NivBook = NivBookParser.fromJson(json)
+
+    println("---------> " + (nivBook == nivBook2))
+  }
 
 }
